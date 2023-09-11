d50bae0d5c73fbf7bbc10cba1deb3e829ab6a043
Ian Lai <a186235@gmail.com>
2023-09-11 18:14:00 +0800

    feat: add new version of count and groupByKey functions without using set

--- week-2/assignment-3/assignment-3.js
+++ week-2/assignment-3/assignment-3.js
@@ -1,38 +1,70 @@
-function count(input) {
+// function count(input) {
+//   let num = {};
+//   let uni = new Set();
+//   let length = uni.size;
+
+//   input.forEach((el) => {
+//     uni.add(el);
+
+//     if (uni.size > length) {
+//       num[el] = 1;
+//       length++;
+//     } else {
+//       num[el]++;
+//     }
+//   });
+//   return num;
+// }
+
+function countV2(input) {
   let num = {};
-  let uni = new Set();
-  let length = uni.size;
+
   input.forEach((el) => {
-    uni.add(el);
-    if (uni.size > length) {
+    if (num[el] === undefined) {
       num[el] = 1;
-      length++;
     } else {
       num[el]++;
     }
   });
   return num;
 }
+
 let input1 = ["a", "b", "c", "a", "c", "a", "x"];
-console.log(count(input1));
+// console.log(count(input1));
+console.log(countV2(input1));
 // should print {a:3, b:1, c:2, x:1}
-function groupByKey(input) {
+
+// function groupByKey(input) {
+//   let num = {};
+//   let uni = new Set();
+//   let length = uni.size;
+
+//   input.forEach((el) => {
+//     uni.add(el.key);
+
+//     if (uni.size > length) {
+//       num[el.key] = el.value;
+//       length++;
+//     } else {
+//       num[el.key] += el.value;
+//     }
+//   });
+//   return num;
+// }
+
+function groupByKeyV2(input) {
   let num = {};
-  let uni = new Set();
-  let length = uni.size;
 
   input.forEach((el) => {
-    uni.add(el.key);
-    console.log(uni.size, length);
-    if (uni.size > length) {
+    if (num[el.key] === undefined) {
       num[el.key] = el.value;
-      length++;
     } else {
       num[el.key] += el.value;
     }
   });
   return num;
 }
+
 let input2 = [
   { key: "a", value: 3 },
   { key: "b", value: 1 },
@@ -40,5 +72,7 @@ let input2 = [
   { key: "a", value: 3 },
   { key: "c", value: 5 },
 ];
-console.log(groupByKey(input2));
+
+// console.log(groupByKey(input2));
+console.log(groupByKeyV2(input2));
 // should print {a:6, b:1, c:7}
