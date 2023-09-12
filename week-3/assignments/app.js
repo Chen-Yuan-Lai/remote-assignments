e3e4fcf352c7fe0c717639e56dd0e0cfd6768bcb
Ian Lai <a186235@gmail.com>
2023-09-12 15:33:45 +0800

    feat: set up some middleware to process  requests and response
    1. Set view engine and views folder path
    2. Set cookie parser to parse cookie information

--- week-3/assignments/app.js
+++ week-3/assignments/app.js
@@ -1,8 +1,18 @@
+const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const sumRouter = require('./routes/sumRoute.js');
 
 const app = express();
 const port = 3000;
+
+// set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// cookie parser
+app.use(cookieParser());
 
 // serve static files
 app.use(express.static('public'));
