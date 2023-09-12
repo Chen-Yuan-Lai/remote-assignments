875a278b1d4b21db358be58c30550874514441d1
Ian Lai <a186235@gmail.com>
2023-09-12 12:31:41 +0800

    fix: modify reqire path

--- week-3/assignments/routes/sumRoute.js
+++ week-3/assignments/routes/sumRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sumController = require('../controllers/sumController.js');
+const sumController = require('../controllers/controller.js');
 
 const router = express.Router();
 
