73ec8da940176c0bd83c4c29caaccf99972d2bfe
Ian Lai <a186235@gmail.com>
2023-09-14 18:28:34 +0800

    feat: add fetch API version for get data from backend

--- week-3/assignments/public/sum.js
+++ week-3/assignments/public/sum.js
@@ -1,18 +1,32 @@
 const calcuForm = document.querySelector('.input-form');
+const ans = document.querySelector('.ans');
 
-const xhttp = new XMLHttpRequest();
+function ajax(url) {
+  const xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    const ans = document.querySelector('.ans');
-    ans.textContent = `Ans: ${this.responseText}`;
-  }
-};
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      ans.textContent = `Ans: ${this.responseText}`;
+    }
+  };
+
+  xhttp.open('GET', url, true);
+  xhttp.send();
+}
+
+async function fet(url) {
+  const response = await fetch(url);
+  const sum = await response.json();
+
+  ans.textContent = `Ans: ${sum}`;
+}
 
 calcuForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const num = document.getElementById('integer').value;
   const url = `http://localhost:3000/data?number=${num}`;
-  xhttp.open('GET', url, true);
-  xhttp.send();
+
+  // ajax(url)
+
+  fet(url);
 });
--- week-3/assignments/public/track.js
+++ week-3/assignments/public/track.js
@@ -1,18 +1,32 @@
 const form = document.querySelector('.input-form');
 
-const xhttp = new XMLHttpRequest();
+const myNameUrl = 'http://localhost:3000/myName';
 
-xhttp.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    window.location.href = 'http://localhost:3000/myName';
-  }
-};
+function ajax(url) {
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      window.location.href = myNameUrl;
+    }
+  };
+
+  xhttp.open('GET', url, true);
+  xhttp.send();
+}
+
+async function fet(url) {
+  const response = await fetch(url);
+
+  window.location.href = myNameUrl;
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.getElementById('name').value;
-  console.log(name);
   const url = `http://localhost:3000/trackName?name=${name}`;
-  xhttp.open('GET', url, true);
-  xhttp.send();
+
+  // ajax(url);
+
+  fet(url);
 });
