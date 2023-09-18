9d302802298c593eab79d66a388f7ff7f6e46b9f
Ian Lai <a186235@gmail.com>
2023-09-18 21:06:48 +0800

    fix: rename esm module import syntx

--- week-4/assignment-3/app.js
+++ week-4/assignment-3/app.js
@@ -1,14 +1,15 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+import createError from "http-errors";
+import express from "express";
+import path, { dirname } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "node:url";
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+import router from "./routes/index.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,8 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -39,6 +39,5 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+export default app;
