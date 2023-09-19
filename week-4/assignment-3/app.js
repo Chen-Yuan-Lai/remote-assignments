d61dc87e259e83f89b815fad72e8beaa254a89bc
Ian Lai <a186235@gmail.com>
2023-09-19 18:28:13 +0800

    refactor: moduling app.js and listening request in server.js

--- week-4/assignment-3/app.js
+++ week-4/assignment-3/app.js
@@ -1,24 +1,31 @@
 import createError from "http-errors";
 import express from "express";
+import cors from "cors";
 import path, { dirname } from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { fileURLToPath } from "node:url";
 
-import router from "./routes/index.js";
+import router from "./routes/mainRouter.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
+
+// Enable CORS
+app.use(cors());
+
+//Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// static file
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", router);
@@ -38,6 +45,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
 
 export default app;
