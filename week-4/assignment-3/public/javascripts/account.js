d9d7600db11000aebf685fe2cb5509528545284c
Ian Lai <a186235@gmail.com>
2023-09-20 21:42:52 +0800

    feat: create helper functions to decouple main functionality of home page
    1. fetchData: send POST request from sign-in or sign-out form
    2. getFormData: collect user input from sign-in or sign-up form
    3. showMessage: show an error message for a short time if some error occur.

---
+++ week-4/assignment-3/public/javascripts/account.js
@@ -0,0 +1,46 @@
+// send post request to backend
+export async function fetchData(url, data) {
+  try {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, requestOptions);
+
+    if (!response.ok && !`${response.status}`.startsWith("4")) {
+      console.error(response.status);
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("There was a problem with the POST request:", error);
+  }
+}
+
+export function getFormData(form) {
+  const emailField = form.querySelector(".data-email");
+  const passwordField = form.querySelector(".data-password");
+  const email = emailField.value;
+  const password = passwordField.value;
+
+  emailField.value = "";
+  passwordField.value = "";
+
+  return { email, password };
+}
+
+export function showMessage(message, status, description, url) {
+  if (status === "success") {
+    window.location.href = `${url}member`;
+    return;
+  }
+
+  message.style.display = "block";
+  message.textContent = description;
+
+  setTimeout(() => (message.style.display = "none"), 2000);
+}
