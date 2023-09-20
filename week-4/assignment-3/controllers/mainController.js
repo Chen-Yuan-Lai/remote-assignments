d527f45eb58106f6fee879ded2e87efd59f36297
Ian Lai <a186235@gmail.com>
2023-09-20 21:55:58 +0800

    refactor: decopule sign-in and sign-up functionalities to simple independent functions

--- week-4/assignment-3/controllers/mainController.js
+++ week-4/assignment-3/controllers/mainController.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+import { AppError } from "../util/appError.js";
 
 dotenv.config();
 
@@ -13,101 +14,52 @@ const pool = mysql
   })
   .promise();
 
-async function getUser(email) {
-  const [rows] = await pool.query(
-    `
-    SELECT * 
-    FROM user 
-    WHERE email= ?
-    `,
-    [email]
-  );
+async function getOneUser(req, res, next) {
+  try {
+    const { email } = req.body;
 
-  return rows[0];
+    const [row] = await pool.query(
+      `
+      SELECT * 
+      FROM user 
+      WHERE email= ?
+      `,
+      [email]
+    );
+
+    if (!row.length) {
+      return next(new AppError("No user found with that email", 404));
+    }
+
+    res.status(201).json({
+      status: "success",
+      data: row,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function createUser(email, password) {
-  await pool.query(
-    `
+async function createUser(req, res, next) {
+  try {
+    const data = req.body;
+
+    await pool.query(
+      `
     INSERT INTO user (email, password)
     VALUES (?, ?)
     `,
-    [email, password]
-  );
-}
-
-function homePage(req, res) {
-  res.render("index");
-}
-
-function member(req, res) {
-  res.render("member");
-}
-
-async function signUp(req, res) {
-  try {
-    const { email, password } = req.body;
-    let status;
-    let description;
-
-    if (email === "" || password === "") {
-      status = "fail";
-      description = "email or password can't be empty";
-    } else {
-      const result = await getUser(email);
-
-      console.log(result);
-
-      // account not exist in the database
-      if (result === undefined) {
-        // create new user in the database
-        await createUser(email, password);
-        status = "success";
-        description = "create a new user";
-      } else {
-        status = "fail";
-        description = "This user has already exsited";
-      }
-    }
+      [data.email, data.password]
+    );
 
     res.status(201).json({
-      status,
-      description,
+      status: "success",
+      data: data,
     });
   } catch (error) {
     console.error(error.message);
+    next(error);
   }
 }
 
-async function signIn(req, res) {
-  try {
-    const { email, password } = req.body;
-    let status;
-    let description;
-
-    if (email === "" || password === "") {
-      status = "fail";
-      description = "email or password can't be empty";
-    } else {
-      const result = await getUser(email);
-
-      if (result === undefined) {
-        // create new user in the database
-        status = "fail";
-        description = "Not found, please signing up";
-      } else {
-        status = "success";
-        description = "sign-in successfully!";
-      }
-    }
-
-    res.status(201).json({
-      status,
-      description,
-    });
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-export default { homePage, signUp, signIn, member };
+export default { getOneUser, createUser };
