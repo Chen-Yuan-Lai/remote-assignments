bca148bd168be616b037c3d0315e5381afae4e27
Ian Lai <a186235@gmail.com>
2023-09-20 11:12:49 +0800

    feat: write a script to insert fake data into article table automatically

---
+++ week-4/assignment-4/inputPosts.js
@@ -0,0 +1,43 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// connection database
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+async function fetchPosts(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function insertPosts() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const posts = await fetchPosts(url);
+
+  const insertPostsPromise = posts.map(async (el) => {
+    const author = Math.floor(Math.random() * 3 + 1);
+
+    await pool.query(
+      `
+      INSERT INTO article (author, title, content)
+      VALUES(?, ?, ?)
+      `,
+      [author, el.title, el.body]
+    );
+  });
+
+  await Promise.all(insertPostsPromise);
+  pool.end();
+  console.log("Complete");
+  return;
+}
+
+insertPosts();
