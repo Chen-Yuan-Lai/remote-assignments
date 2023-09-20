12988100734131c3f547d6318caa21bd38d7b642
Ian Lai <a186235@gmail.com>
2023-09-20 21:51:26 +0800

    fix: rewrite error handler middleware

--- week-4/assignment-3/app.js
+++ week-4/assignment-3/app.js
@@ -7,6 +7,7 @@ import logger from "morgan";
 import { fileURLToPath } from "node:url";
 
 import router from "./routes/mainRouter.js";
+import { globalErrorHandler } from "./controllers/errorController.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -36,14 +37,6 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.use(globalErrorHandler);
 
 export default app;
