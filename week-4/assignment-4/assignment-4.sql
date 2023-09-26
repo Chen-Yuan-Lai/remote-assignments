91853dc85027306514b564a9079fd640379176e0
Cheng-Yuan-Lai <a186235@gmail.com>
2023-09-26 14:56:47 +0800

    fix: modify SQL statement in assignment-4

--- week-4/assignment-4/assignment-4.sql
+++ week-4/assignment-4/assignment-4.sql
@@ -1,7 +1,7 @@
 /* select all articles with their author’s email */
-SELECT email, title, content
-FROM user
-INNER JOIN article ON user.id=article.author;
+SELECT title, content, email
+FROM article
+LEFT OUTER JOIN user ON article.author=user.id;
 
 /* select articles from 7th to 12th sorted by id */
 SELECT article.id, email, title
