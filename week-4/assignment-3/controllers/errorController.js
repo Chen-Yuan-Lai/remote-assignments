2b265b5d6189129246ea80f7b4d5ab97837e3841
Cheng-Yuan-Lai <a186235@gmail.com>
2023-09-26 14:41:40 +0800

    refactor: remove duplicate error handler in global error handler

--- week-4/assignment-3/controllers/errorController.js
+++ week-4/assignment-3/controllers/errorController.js
@@ -1,20 +1,9 @@
-import { AppError } from "../util/appError.js";
-
-function handleDuplicateFieldsDB(err) {
-  const message = "Duplicate email! Please sign-up again";
-  return new AppError(message, 400);
-}
-
 export function globalErrorHandler(err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "fail";
 
-  let error = Object.assign(err);
-
-  if (err.errno === 1062) error = handleDuplicateFieldsDB(err);
-
-  res.status(error.statusCode).json({
-    status: error.status,
-    data: error.message,
+  res.status(err.statusCode).json({
+    status: err.status,
+    data: err.message,
   });
 }
